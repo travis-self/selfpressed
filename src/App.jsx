12a939888d8afd64d6ece7dd3f7f1ab9fb7422e7
Travis Self <tdself87@gmail.com>
2025-03-13 23:24:02 -0500

    Adding some x-padding to <main> elem.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <main className="max-w-3xl mx-auto pt-20 relative">
+      <main className="max-w-3xl mx-auto pt-20 px-10 relative">
         <Heading as="h1">Self Pressed.</Heading>
         <p className="mt-10">
           I’m <Anchor href="http://linkedin.com/in/selfpressed" target="_blank">Travis Self</Anchor>, a web developer and podcaster from Dallas. I build websites and lead a small team of Front End Engineers at <Anchor href="http://lifeblue.com/" target="_blank">Lifeblue</Anchor>, where we enable companies to grow their social, cultural and economic impact in the world.
