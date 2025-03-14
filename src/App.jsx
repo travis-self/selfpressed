3086cde163ae538576575827a762d7140c259154
Travis Self <tdself87@gmail.com>
2025-03-14 00:21:26 -0500

    Adding a simple footer component. Removing heavy-handed paragraph styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import Heading from "./atoms/Heading"
 import Anchor from "./atoms/Anchor"
+import Footer from "./molecules/Footer"
+import Heading from "./atoms/Heading"
 
 function App() {
 
   return (
     <>
-      <main className="max-w-3xl mx-auto pt-20 px-10 relative">
+      <main className="pt-20 px-10 relative wrapper">
         <Heading as="h1">Self Pressed.</Heading>
-        <p className="mt-10">
+        <p className="mt-10 text-clamp-sm">
           I’m <Anchor href="http://linkedin.com/in/selfpressed" target="_blank">Travis Self</Anchor>, a web developer and podcaster from Dallas. I build websites and lead a small team of Front End Engineers at <Anchor href="http://lifeblue.com/" target="_blank">Lifeblue</Anchor>, where we enable companies to grow their social, cultural and economic impact in the world.
         </p>
-        <p>
+        <p className="text-clamp-sm">
           You can hear me chat about music on <Anchor href="https://cms.megaphone.fm/channel/nofiller" target="_blank">No Filler</Anchor>, and interview web devs and tech professionals on <Anchor href="https://soundcloud.com/600px" target="_blank">600px</Anchor>.
         </p>
       </main>
+      <Footer />
     </>
   )
 }
