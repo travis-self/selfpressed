7095790604f812d65e2c954edd056d99256f89af
Travis Self <tdself87@gmail.com>
2025-03-13 23:22:56 -0500

    Opting for clamp() to make font sizing more fluid.

--- src/atoms/Heading.jsx
+++ src/atoms/Heading.jsx
@@ -1,7 +1,7 @@
 export default function Heading(props) { 
   const { as: Tag = 'h1', children } = props
   const styles = {
-    h1: 'text-8xl text-shadow text-transparent'
+    h1: 'text-clamp text-shadow text-transparent'
   }
 
   return <Tag className={`font-display ${styles[Tag]} text-`}>{children}</Tag>
