4e88c6a5b96e90fd4def32f535dd12b66f38fe5b
Travis Self <tdself87@gmail.com>
2025-03-23 18:51:47 -0500

    Abandoning Wordnik API for now.

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode}) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    define: {
-      'process.env': {}
-    },
-    plugins: [
-      react(),
-      tailwindcss()
-    ],
-  }
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 })
