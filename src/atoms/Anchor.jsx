db1f9e8370db0e8591335e6cad25f7119ab5ae90
Travis Self <tdself87@gmail.com>
2026-02-15 17:20:31 -0600

    Adding a variant to Anchor that does not add animate line class and just changes text color on hover.

--- src/atoms/Anchor.jsx
+++ src/atoms/Anchor.jsx
@@ -1,7 +1,12 @@
 import { Link } from 'react-router-dom';
 
-export default function Anchor({children, className, href, target}) {
-  const classes = `animate-line font-bold relative dark:text-cyan-300 ${className || ''}`
+export default function Anchor({animate = true, children, className, href, target}) {
+let classes = `font-bold relative dark:text-cyan-300 ${className || ''}`
+  if (animate) {
+    classes += ' animate-line'
+  } else {
+    classes += ' hover:text-indigo-500'
+  }
 
   return <Link className={ classes } to={href} target={target}>
     {children}
