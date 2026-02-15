f03402a2a58a7bfa25b1459528d8c6f180939366
Travis Self <tdself87@gmail.com>
2026-02-15 17:56:51 -0600

    Set scroll pos to top when navigating via Link router.

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./Footer";
 import Nav from "./Nav";
 
 export default function Layout() {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location.pathname])
+
   return (
     <>
       <Nav />
